ede175d8334fdbcdd5d3beaae1dd9b87c45eaceb
sakutaro220 <33340195+sakutaro220@users.noreply.github.com>
2019-05-27 21:05:17 +0900

    c0ban ネットワークパラメータ設定

--- lib/bitcoin.rb
+++ lib/bitcoin.rb
@@ -926,7 +926,54 @@ module Bitcoin
       reset_target_block: 157500,
       auxpow_start_block: 158100
     })
-
+  NETWORKS[:c0ban] = NETWORKS[:bitcoin].merge({
+    project: :c0ban,
+    magic_head: "\x63\x30\x62\x6E",
+    message_magic: "c0ban Signed Message:\n",
+    address_version: "12",
+    p2sh_version: "1c",
+    privkey_version: "88",
+    bech32_hrp: nil,
+    extended_privkey_version: "0488ade4",
+    extended_pubkey_version: "0488b21e",
+    default_port: 3881,
+    protocol_version: 70015,
+    max_money: 880_000_000 * COIN,
+    min_tx_fee: COIN,
+    min_relay_tx_fee: COIN,
+    free_tx_bytes: 1_000,
+    dust: COIN,
+    per_dust_fee: true,
+    coinbase_maturity: 30,
+    coinbase_maturity_new: 100,
+    reward_base: 1 * COIN,
+    reward_halving: 100_000,
+    retarget_interval: 1,
+    retarget_time: 32, # 4 hours
+    retarget_time_new: 32, # 1 minute
+    target_spacing: 32, # block interval
+    dns_seeds: [
+      "jp01.dnsseed.c0ban.com"
+    ],
+    genesis_hash: "000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8",
+    proof_of_work_limit: 0x1d00ffff,
+    known_nodes: [],
+    checkpoints: {
+      11111 => "0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d",
+      33333 => "000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6",
+      74000 => "0000000000573993a3c9e41ce34471c079dcf5f52a0e824a81e7f953b8661a20",
+      105000 => "00000000000291ce28027faea320c8d2b054b2e0fe44a773f3eefb151d6bdc97",
+      134444 => "00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe",
+      168000 => "000000000000099e61ea72015e79632f216fe6cb33d7899acb35b75c8303b763",
+      193000 => "000000000000059f452a5f7340de6682a977387c17010ff6e6c3bd83ca8b1317",
+      210000 => "000000000000048b95347e83192f69cf0366076336c639f9b7228e9ba171342e",
+      216116 => "00000000000001b4f4b433e81ee46494af945cf96014816a4e2370f11b23df4e",
+      225430 => "00000000000001c108384350f74090433e7fcf79a606b8e797f065b130575932",
+      290000 => "0000000000000000fa0b2badd05db0178623ebf8dd081fe7eb874c26e27d0b3b",
+      300000 => "000000000000000082ccf8f1557c5d40b21edabb18d2d691cfbf87118bac7254",
+      305000 => "0000000000000000142bb90561e1a907d500bf534a6727a63a92af5b6abc6160",
+    },
+  })
   NETWORKS[:namecoin] = NETWORKS[:bitcoin].merge({
       project: :namecoin,
       magic_head: "\xF9\xBE\xB4\xFE",
